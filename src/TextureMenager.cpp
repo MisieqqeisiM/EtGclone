94502ceec85eac70157cc4525f9848f2f79e9484
Krzysztof Pęczek <krzysztofpeczek@protonmail.com>
2019-06-22 22:06:14 +0200

    First version of the texture menager

---
+++ src/TextureMenager.cpp
@@ -0,0 +1,30 @@
+#include "TextureMenager.h"
+
+
+
+TextureMenager::TextureMenager(std::vector<std::string> pathsVector)
+{
+	this->paths = pathsVector;
+	for (std::string path : paths) 
+	{
+		this->textureImages.push_back(LoadTexture(path));
+	}
+}
+
+sf::Image TextureMenager::LoadTexture(const std::string& path)
+{
+	sf::Image image;
+	if (!image.loadFromFile(path)) 
+	{
+		std::cerr << "Couldn't open file: " << path << std::endl;
+	}
+	else 
+	{
+		return image;
+	}
+}
+
+sf::Image* TextureMenager::GetTexturePtr(int id) 
+{
+	return &(this->textureImages[id]);
+}
---
+++ src/TextureMenager.h
@@ -0,0 +1,21 @@
+#ifndef TEXTURE_MENAGER_H
+#define TEXTURE_MENAGER_H
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <string>
+
+class TextureMenager
+{
+private:
+	std::vector<std::string> paths;
+	std::vector<sf::Image> textureImages;
+public:
+	TextureMenager(std::vector<std::string> pathsVector);
+	sf::Image* GetTexturePtr(int id);
+private:
+	sf::Image LoadTexture(const std::string& path);
+};
+
+#endif
